5598ea294b51c12a7ba31ff7356ea98073733132
Pablo <pmacsal2707@g.educaand.es>
2024-10-24 11:30:58 +0200

    codigo

--- tema-04/ejemplos/05/class/class.libro.php
+++ tema-04/ejemplos/05/class/class.libro.php
@@ -30,4 +30,54 @@
             $this->autor = $autor;
             $this->tematica = $tematicas;
         }
+
+        // Getters
+        public function getId() {
+            return $this->id;
+        }
+
+        public function getTitulo() {
+            return $this->titulo;
+        }
+
+        public function getPrecio() {
+            return $this->precio;
+        }
+
+        public function getPaginas() {
+            return $this->paginas;
+        }
+
+        public function getAutor() {
+            return $this->autor;
+        }
+
+        public function getTematica() {
+            return $this->tematica;
+        }
+
+        // Setters
+        public function setId($id) {
+            $this->id = $id;
+        }
+
+        public function setTitulo($titulo) {
+            $this->titulo = $titulo;
+        }
+
+        public function setPrecio($precio) {
+            $this->precio = $precio;
+        }
+
+        public function setPaginas($paginas) {
+            $this->paginas = $paginas;
+        }
+
+        public function setAutor($autor) {
+            $this->autor = $autor;
+        }
+
+        public function setTematica($tematicas) {
+            $this->tematica = $tematicas;
+        }
     }
---
+++ tema-04/ejemplos/05/index.php
@@ -0,0 +1,35 @@
+<?php
+
+    /*
+        Ejemplo 5
+
+        Descripción: uso clase libro con encapsulamiento
+    */
+
+    # Cargo la clase libro
+    include 'class/class.libro.php';
+
+    # Crear un objeto libro vacío
+    $libro1 = new Class_libro();
+
+    # Asignar valores al libro
+    $libro1->setId(1);
+    $libro1->setTitulo('Cien años de soledad');
+    $libro1->setPrecio(10.54);
+    $libro1->setPaginas(25);
+    $libro1->setAutor('Grabiel García Márquez');
+    $libro1->setTematica(['Novela', 'Drama', 'Romance']);
+
+    # Mostrar detalles clase lbro
+    echo 'Id: '. $libro1->getId();
+    echo '<BR>';
+    echo 'Título: '. $libro1->getTitulo();
+    echo '<BR>';
+    echo 'Precio: '. $libro1->getPrecio();
+    echo '<BR>';
+    echo 'Páginas: '. $libro1->getPaginas();
+    echo '<BR>';
+    echo 'Autor: '. $libro1->getAutor();
+    echo '<BR>';
+    echo 'Temática: '. implode(', ', $libro1->getTematica());
+    echo '<BR>';
